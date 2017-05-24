2e8ed92071e047a5d6ca98a82abd2d8f34b32b38
Nikita Soldatov <acacuce2@gmail.com>
2017-05-24 12:14:29 +0300

    add bridging header

---
+++ RamblerAppDelegateProxy/SupportingFiles/RamblerAppDelegateProxy-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  RamblerAppDelegateProxy-Bridging-Header.h
+//  RamblerAppDelegateProxy
+//
+//  Created by Никита Солдатов on 24.05.17.
+//  Copyright © 2017 imc. All rights reserved.
+//
+
+#import "RamblerAppDelegateProxy.h"
+
